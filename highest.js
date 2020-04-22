e6085b5f7c967c13f1f36283221ab482aba89f36
Jesse <jesse.lanum@gmail.com>
2020-04-22 09:37:31 -0700

    updating substring function

--- highest.js
+++ highest.js
@@ -11,16 +11,16 @@ const scoreList = require('fs').readFileSync('./scores.json', 'utf-8'); //Pull l
 const scoreLines = scoreList.toString().split("\n"); // split on the carriage return
 
 const scores = scoreLines.sort(function(x,y){
-    var xp = x.substr(0, 7);
-    var yp = y.substr(0, 7);
+    var xp = x.substr(0,x.indexOf(':'));
+    var yp = y.substr(0,y.indexOf(':'));
     return xp == yp ? 0 : yp < xp ? -1 : 1;
   }); //sort in descending order by first number
 
 const out = (line) => {
-    return fs.appendFileSync('./data.csv',line)
+    return fs.appendFileSync('./data.json',line)
 } // write to data file
 const clean = () => {
-    return fs.writeFileSync('./data.csv','');
+    return fs.writeFileSync('./data.json','');
 }; // clean file from previous run
 
 const limit = process.argv.slice(2); // grab limit from cmd line arg
@@ -28,11 +28,11 @@ const limited = scores.slice(0,limit); // slice score
 
 const format =  async (output) => {
     const finalFormat = [];
-    const formated = {};
+    const formated = [];
     
     for (var i = 0, len = output.length; i < len; i++) {
-        let jsonResult = JSON.parse(output[i].substring(9, output[i].length)); //grab the score
-        let formatted = `{"score":${output[i].substring(0,7)},"id":${jsonResult.id}}`; //grab the json
+        let jsonResult = JSON.parse(output[i].substring(output[i].indexOf('{'), output[i].length)); //grab the score
+        let formatted = `{"score":${output[i].substr(0,output[i].indexOf(':'))},"id":"${jsonResult.id}"}`; //grab the json
         finalFormat.push(formatted);
     }
     
